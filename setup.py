635ae760de191785e433d0797a4cd0de21896e6e
Philip Neustrom <philipn@gmail.com>
2014-04-26 00:40:27 -0700

    + custom2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='django-honeypot',
-    version="0.4.0-custom1",
+    version="0.4.0-custom2",
     package_dir={'honeypot': 'honeypot'},
     packages=['honeypot', 'honeypot.templatetags'],
     description='Django honeypot field utilities',
